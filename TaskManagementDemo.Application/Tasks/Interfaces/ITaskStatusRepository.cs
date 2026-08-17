1551697ab233135facaa1d6ead4f58d5b866d005
Thet Htoo Zaw <thethtoozaw51@gmail.com>
2026-08-17 16:58:04 +0630

    [ADD] task status repo

--- TaskManagementDemo.Application/Tasks/Interfaces/ITaskStatusRepository.cs
+++ TaskManagementDemo.Application/Tasks/Interfaces/ITaskStatusRepository.cs
@@ -1,6 +1,7 @@
 namespace TaskManagementDemo.Application.Tasks.Interfaces;
+using TaskManagementDemo.Domain.Entities;
 
 public interface ITaskStatusRepository
 {
-    
+    Task AddAsync(TaskStatus taskStatus, CancellationToken cancellationToken = default);
 }
--- TaskManagementDemo.Infrastructure/Repositories/TaskStatusRepository.cs
+++ TaskManagementDemo.Infrastructure/Repositories/TaskStatusRepository.cs
@@ -1,6 +1,21 @@
+using TaskManagementDemo.Application.Tasks.Interfaces;
+using TaskManagementDemo.Infrastructure.Persistence;
+using TaskManagementDemo.Domain.Entities;
+
+using TaskStatus= TaskManagementDemo.Domain.Entities.TaskStatus;
+
 namespace TaskManagementDemo.Infrastructure.Repositories;
 
-public class TaskStatusRepository
+public class TaskStatusRepository : ITaskStatusRepository
 {
-    
+    private readonly AppDbContext _dbContext;
+
+    public TaskStatusRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task AddAsync(TaskStatus taskStatus, CancellationToken cancellationToken = default)
+    {
+        await _dbContext.TaskStatuses.AddAsync(taskStatus, cancellationToken);
+    }
 }
